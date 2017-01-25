77edfac93b5b492958760649c8074934ea96ad46
Fingercomp <binbin226@gmail.com>
2017-01-25 12:13:58 +0000

    Add EEPROM implementation

--- zn-eeprom.lua
+++ zn-eeprom.lua
@@ -1,26 +1,7 @@
--- ~ Zn ~ 0.1.0 ~
--- Minimalistic p2p network for OpenComputers
---
--- Copyright 2017 Figercomp, Totoro
---
--- Licensed under the Apache License, Version 2.0 (the "License");
--- you may not use this file except in compliance with the License.
--- You may obtain a copy of the License at
---
---    http://www.apache.org/licenses/LICENSE-2.0
---
--- Unless required by applicable law or agreed to in writing, software
--- distributed under the License is distributed on an "AS IS" BASIS,
--- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
--- See the License for the specific language governing permissions and
--- limitations under the License.
+local com = component
+local comp = computer
 
-
-local event = require("event")
-local com = require("component")
-local computer = require("computer")
-
-local modem = com.modem
+local modem = com.proxy(com.list("modem")())
 local zn = {}
 
 local PORT = 419
@@ -35,14 +16,8 @@ local FLAGS = {
   ACK = "\x50"
 }
 
--- How long does this node remember transferred message hash (in-game seconds)
--- (Used to kill all possible dublicates)
 local HASHLIFETIME = 43200
-
 local isConnected = false
-
-
--- Session ---------------------------------------------------------------------
 
 local hashes = {}
 
@@ -56,14 +31,14 @@ local function hashgen(time, data)
 end
 
 local function check(hash)
-  local time = computer.uptime()
+  local time = comp.uptime()
   for k, v in pairs(hashes) do
     if time - v > HASHLIFETIME then
       hashes[k] = nil
     end
   end
   if not hashes[hash] then
-    hashes[hash] = computer.uptime()
+    hashes[hash] = comp.uptime()
     return true
   end
   return hashes[hash] == nil
@@ -99,7 +74,7 @@ end
 
 local function send(selfAddress, address, message, hash, code, flags)
   local hash = hash or hashgen(getTime(), message)
-  hashes[hash] = computer.uptime()
+  hashes[hash] = comp.uptime()
   modem.broadcast(PORT, code, address, selfAddress, hash, flags, message)
 end
 
@@ -112,23 +87,23 @@ local function listener(name, receiver, sender, port, distance,
         code == CODES.ping or
         code == CODES.ack) then
       if code == CODES.ping then
-        computer.pushSignal("zn_ping", sender, distance)
+        comp.pushSignal("zn_ping", sender, distance)
         modem.send(sender, PORT, CODES.pong)
         return true
       end
       if code == CODES.pong then
-        computer.pushSignal("zn_pong", sender, distance)
+        comp.pushSignal("zn_pong", sender, distance)
         return true
       end
       if check(hash) then
         if recvAddr == zn.modem.address or recvAddr == "" then
           if code == CODES.send then
-            computer.pushSignal("zn_message", body, recvAddr, sendAddr)
+            comp.pushSignal("zn_message", body, recvAddr, sendAddr)
             if recvAddr == receiver and parseFlags(flags).ACK then
               send(zn.modem.address, sendAddr, hash, nil, CODES.ack, packFlags {})
             end
           elseif code == CODES.ack then
-            computer.pushSignal("zn_ack", body, recvAddr, sendAddr)
+            comp.pushSignal("zn_ack", body, recvAddr, sendAddr)
           end
         end
         if recvAddr ~= zn.modem.address then
@@ -139,30 +114,7 @@ local function listener(name, receiver, sender, port, distance,
   end
 end
 
-zn.connect = function()
-  if isConnected then
-    return false
-  end
-  math.randomseed(getTime())
-  modem.open(PORT)
-  event.listen("modem_message", listener)
-  isConnected = true
-  return true
-end
-
-zn.disconnect = function()
-  if not isConnected then
-    return false
-  end
-  modem.close(PORT)
-  event.ignore("modem_message", listener)
-  isConnected = false
-  return true
-end
-
 zn.modem = com.modem
-
--- Messages --------------------------------------------------------------------
 
 zn.send = function(address, message, timeout)
   local flags = {ACK = true}
@@ -188,4 +140,11 @@ zn.ping = function()
   modem.broadcast(PORT, CODES.ping)
 end
 
-return zn
+math.randomseed(getTime())
+modem.open(PORT)
+while true do
+  local e = {comp.pullSignal()}
+  if e[1] == "modem_message" then
+    listener(table.unpack(e))
+  end
+end
